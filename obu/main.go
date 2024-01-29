55b5f183bce4e9587f85e2582695c135bcbd329c
bseleng <bogdan.selenginskiy@multilogin.com>
2024-01-29 17:34:25 +0300

    feat: add data reciever

--- obu/main.go
+++ obu/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math"
 	"math/rand"
 	"time"
+
+	"github.com/bseleng/tolling/types"
+	"github.com/gorilla/websocket"
 )
 
-var sendInterval = time.Second
+const wsEndpoint = "ws://127.0.0.1:30000/ws"
 
-type OBUData struct {
-	OBUID int     `json:"obuID"`
-	Lat   float64 `json:"lat"`
-	Long  float64 `json:"long"`
-}
+var sendInterval = time.Second
 
 func genCoord() float64 {
 	n := float64(rand.Intn(100) + 1)
@@ -27,17 +26,23 @@ func genLatLong() (float64, float64) {
 
 func main() {
 	obuIDS := generateOBUIDS(20)
-
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 	for {
 
 		for i := 0; i < len(obuIDS); i++ {
 			lat, long := genLatLong()
-			data := OBUData{
+			data := types.OBUData{
 				OBUID: obuIDS[i],
 				Lat:   lat,
 				Long:  long,
 			}
-			fmt.Printf("%+v\n", data)
+
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
 		}
 
 		time.Sleep(sendInterval)
